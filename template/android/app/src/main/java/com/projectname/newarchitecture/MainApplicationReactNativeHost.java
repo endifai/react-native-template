8e03321d93eae98d7773100ebce707722137edc0
endifai <dimasik.uva@yandex.ru>
2022-08-17 18:26:59 +0400

    fix: template

--- template/android/app/src/main/java/com/projectname/newarchitecture/MainApplicationReactNativeHost.java
+++ template/android/app/src/main/java/com/projectname/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.app.newarchitecture;
+package com.projectname.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.app.BuildConfig;
-import com.app.newarchitecture.components.MainComponentsRegistry;
-import com.app.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.projectname.BuildConfig;
+import com.projectname.newarchitecture.components.MainComponentsRegistry;
+import com.projectname.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
